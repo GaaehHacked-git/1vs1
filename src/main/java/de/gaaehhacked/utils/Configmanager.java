88750c43c7fe83c1a9a1af0252b810195d7fd364
GaaehHacked-git <71274777+GaaehHacked-git@users.noreply.github.com>
2021-11-15 22:43:02 +0100

    ConfigManager changes

--- src/main/java/de/gaaehhacked/utils/Configmanager.java
+++ src/main/java/de/gaaehhacked/utils/Configmanager.java
@@ -3,6 +3,7 @@ package de.gaaehhacked.utils;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Configmanager  {
 
@@ -12,5 +13,26 @@ public class Configmanager  {
     public Configmanager(String filename){
         file = new File("plugins//1vs1//configs//" + filename + ".yml");
         cfg = YamlConfiguration.loadConfiguration(file);
+        file.mkdirs();
+    }
+
+    public void create() throws IOException {
+        if(!exists()){
+            file.createNewFile();
+        }
+    }
+
+    public boolean exists(){
+        if(file.exists()){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public void delete(){
+        if(exists()){
+            file.delete();
+        }
     }
 }
