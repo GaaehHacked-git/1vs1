6881c7ee96f09ca794e171a6d3e1282d4a3e4818
GaaehHacked-git <71274777+GaaehHacked-git@users.noreply.github.com>
2021-11-15 22:30:54 +0100

    ConfigManager added

--- src/main/java/de/gaaehhacked/utils/Configmanager.java
+++ src/main/java/de/gaaehhacked/utils/Configmanager.java
@@ -8,4 +8,9 @@ public class Configmanager  {
 
     File file;
     YamlConfiguration cfg;
+
+    public Configmanager(String filename){
+        file = new File("plugins//1vs1//configs//" + filename + ".yml");
+        cfg = YamlConfiguration.loadConfiguration(file);
+    }
 }
